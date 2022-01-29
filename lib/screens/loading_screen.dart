e533ba20934195e32ee88c7bb5f84ddad86d631c
Bharat8448 <bpamnani35@gmail.com>
2022-01-29 19:58:11 +0530

    climate app

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
+
+
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,15 +14,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+WeatherModel weatherModel=WeatherModel();
+   void getLocationdata() async{
+var weatherdata=await weatherModel.locationweather();
+   Navigator.push(context, MaterialPageRoute(builder: (context){
+     return LocationScreen(weatherdata);
+   }));
+
+
+
+  }
+
+
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationdata();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,30 @@
+import 'package:clima/services/location.dart ';
+import 'package:clima/services/networking.dart';
+import 'package:clima/screens/loading_screen.dart';
+const apikey='1c71168bcb1515cc15e5cb4bfe702777';
+
 class WeatherModel {
+  double long;
+  double lati;
+  Future<dynamic>getcityweather (String cityName) async{
+
+    Network network=Network('https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apikey&units=metric');
+    var weatherdata=await network.getdata();
+return weatherdata;
+  }
+
+  Future<dynamic> locationweather() async{
+
+    Location location=Location();
+    await location.getcurrentlocation();
+    long=location.longitude;
+    lati=location.latitude;
+    Network network=Network('https://api.openweathermap.org/data/2.5/weather?lat=$lati&lon=$long&appid=$apikey&units=metric');
+
+    var weatherdata= await network.getdata();
+    return weatherdata;
+
+  }
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
